da1ffa2066e19596c56c21854bc1d52c34fa12d2
Andrea Giulianelli <andrygiulio@gmail.com>
2023-12-24 12:13:33 +0100

    build: set main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,3 +54,7 @@ tasks.withType<Test> {
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
 }
+
+application {
+    mainClass.set("io.github.webbasedwodt.Launcher")
+}
